990b75af218d43b5328f168bfffef1422d323cb9
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2021-12-27 00:51:44 +0300

    dynamically adjust tickrate in everythingfucker.js for MAX EFFICIENCY

--- everythingfucker.js
+++ everythingfucker.js
@@ -9,10 +9,11 @@ export async function main(ns) {
 	const neverScan = ['home', 'darkweb'];
 	const updateInterval = 500;
 	const minChance = 0.6; // min chance before proceeding to grow/hack
-	const minMoney = 1000; // min money on a server before proceeding to hack
+	const minMoney = 500000; // min money on a server before proceeding to hack
 	const minMoneyPercentage = 0.5; // caps out the above number to x * maxmoney
-	const maxThreads = 16; // max threads to allocate per site to hack
+	const maxThreads = 32; // max threads to allocate per site to hack
 	const maxLogLength = 10;
+	const prioritizeTimeBy = 0.3; // how much to prioritize time in ordering servers [0.0 : 1.0]
 
 	// scanning recursion-avoiding stuff
 	let memory = [];
@@ -29,6 +30,9 @@ export async function main(ns) {
 	// temporary caches ( reset on tick start )
 	let moneyAvailableCache = {};
 	let hackChanceCache = {};
+	let hackTimeCache = {};
+	let weakenTimeCache = {};
+	let growTimeCache = {};
 
 	// misc
 	let processesPerTick = 0;
@@ -124,6 +128,29 @@ export async function main(ns) {
 	function getMaxRam(hostname) {
 		return maxRamCache[hostname] || (maxRamCache[hostname] = ns.getServerMaxRam(hostname));
 	}
+	function getHackTime(hostname) {
+		return hackTimeCache[hostname] || (hackTimeCache[hostname] = ns.getHackTime(hostname));
+	}
+	function getWeakenTime(hostname) {
+		return weakenTimeCache[hostname] || (weakenTimeCache[hostname] = ns.getWeakenTime(hostname));
+	}
+	function getGrowTime(hostname) {
+		return growTimeCache[hostname] || (growTimeCache[hostname] = ns.getGrowTime(hostname));
+	}
+
+	function getAvgTime(hostname) {
+		return (getHackTime(hostname) + getWeakenTime(hostname) + getGrowTime(hostname)) / 3;
+	}
+
+	function formatTime(ms) {
+		let minutes = 0;
+		let secs = ms / 1000;
+
+		minutes += secs / 60;
+		secs = secs % 60;
+
+		return `${minutes <= 0 ? `${Math.floor(minutes)}m ` : ''}${secs.toPrecision(3)}`;
+	}
 
 	// initialize stuff
 	await ns.write('grow.script', 'grow(args)', 'w');
@@ -138,6 +165,11 @@ export async function main(ns) {
 		// reset caches
 		moneyAvailableCache = {};
 		hackChanceCache = {};
+		hackTimeCache = {};
+		weakenTimeCache = {};
+		growTimeCache = {};
+
+		let smallestTimeToWait = updateInterval;
 
 		// hackfucker.js portion
 		let exes = getExes();
@@ -156,7 +188,10 @@ export async function main(ns) {
 		let rooted = res[0];
 		let sorted = rooted
 			.filter(s => getMaxMoneyAvailable(s) > 0) // avoid servers with no money at all
-			.sort((s2, s1) => getMaxMoneyAvailable(s1) - getMaxMoneyAvailable(s2));
+			.sort((s2, s1) =>
+				getMaxMoneyAvailable(s1) * (getAvgTime(s1) * prioritizeTimeBy + (1 - prioritizeTimeBy)) -
+				getMaxMoneyAvailable(s2) * (getAvgTime(s2) * prioritizeTimeBy + (1 - prioritizeTimeBy))
+			);
 
 		let totalStartedScripts = 0;
 		let totalRunningScripts = 0;
@@ -185,12 +220,16 @@ export async function main(ns) {
 				const minMoneyAmt = Math.min(minMoney, minMoneyPercentage * getMaxMoneyAvailable(targetServer));
 
 				let command;
+				let time;
 				if (hackChance < minChance) {
 					command = 'weaken.script';
+					time = getWeakenTime(targetServer);
 				} else if (getMoneyAvailable(targetServer) < minMoneyAmt) { // penis
 					command = 'grow.script';
+					time = getGrowTime(targetServer);
 				} else {
 					command = 'hack.script';
+					time = getHackTime(targetServer);
 				}
 
 				if (ns.isRunning(command, fromServer, targetServer)) continue;
@@ -200,9 +239,12 @@ export async function main(ns) {
 				let threads = Math.min(Math.floor(availram / scriptram), maxThreads);
 				if (scriptram > availram) break;
 
+				time /= threads;
+
 				const pid = ns.exec(command, fromServer, threads, targetServer);
 				if (pid === 0) break;
-				logs.push(`+ ${fromServer} : ${command.split('.')[0]} ${targetServer}, t = ${threads}`);
+				smallestTimeToWait = Math.min(smallestTimeToWait, time);
+				logs.push(` ${fromServer} : ${command.split('.')[0]} ${targetServer}, t = ${threads} | est time ${formatTime(time)}s`);
 				scriptsRan++;
 			}
 
@@ -228,7 +270,7 @@ export async function main(ns) {
 		let avgThreadCount = threadCounts.reduce((p, c) => p + c) / threadCounts.length;
 		processesPerTick = Math.max(processesPerTick * 0.95, totalStartedScripts);
 
-		if (logs.length > maxLogLength) logs = logs.slice(-maxLogLength, -1);
+		if (logs.length > maxLogLength) logs = logs.slice(-maxLogLength);
 
 		ns.print(`${logs.join('\n')}
 
@@ -236,10 +278,10 @@ ${totalRunningScripts} processes running on ${rooted.length} servers hacking ${s
  ${totalStartedScripts} processes (${Math.floor(totalStartedScripts / totalRunningScripts * 1000) / 10}%) started this tick
  ${ranHack} hacking, ${ranWeaken} weakening, ${ranGrow} growing
  ${Math.floor(avgThreadCount * 10) / 10} threads avg, ${Math.floor(totalRunningScripts / sorted.length * 10) / 10} processes per server avg
- ${Math.floor(processesPerTick / (updateInterval / 1000) * 100) / 100} processes / s
+ ${Math.floor(processesPerTick / (smallestTimeToWait / 1000) * 100) / 100} processes / s; waiting ${(smallestTimeToWait / 1000).toFixed(2)}s before next tick
 
 everythingfucker.js | (ↄ) Jill "oatmealine" Monoids 2021`);
 
-		await ns.sleep(updateInterval);
+		await ns.sleep(smallestTimeToWait + 100); // to prevent weird timing jank
 	}
 }
