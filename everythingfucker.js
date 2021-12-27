14fbab924501badbee0ebb825663f55a76de2de2
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2021-12-28 00:43:02 +0300

    everythingfucker scheduling
    this will predict how money and security level will change over time and adjust to that. this is as optimized as im able to make this with my current amount of coding knowledge, i believe

--- everythingfucker.js
+++ everythingfucker.js
@@ -1,7 +1,7 @@
 // everythingfucker.js
 // usage: simply run it
 // its recommended to use fuckerdaemon.js, but it also runs standalone at a bigger amount of ram
-// takes up 5.90GB of ram, automatically hacks any servers that are hackable, tries to avoid unnecessary calls with lots of caching
+// takes up 6.10GB of ram, automatically hacks any servers that are hackable, tries to avoid unnecessary calls with lots of caching
 // you may want to mess with the constants if youre getting bad results!
 
 // constants!
@@ -27,16 +27,25 @@ let moneyMaxCache = {};
 let scriptMemoryCache = {};
 let maxRamCache = {};
 let requiredHackingLevel = {};
+let securityMinLevelCache = {};
 // temporary caches ( reset on tick start )
 let moneyAvailableCache = {};
 let hackChanceCache = {};
 let hackTimeCache = {};
 let securityLevelCache = {};
 let playerCache;
+let growthCache = {};
+
+// scheduling! :)
+// [hostname, changeBy, when]
+let securityLevelScheduled = [];
+let moneyAvailableScheduled = [];
 
 // misc
 let processesPerTick = 0;
 let logs = [];
+let t = 0; // for scheduling
+let lastt = Date.now();
 
 function scan(ns, servers) {
 	let lServers = [];
@@ -121,75 +130,162 @@ function nukeAllNukable(ns, exes) {
 // formulas.js moment
 
 function calculateIntelligenceBonus(intelligence, weight = 1) {
-  return 1 + (weight * Math.pow(intelligence, 0.8)) / 600;
+	return 1 + (weight * Math.pow(intelligence, 0.8)) / 600;
 }
 
 /** @param {NS} ns **/
 function hackTime(ns, hostname) {
-  const difficultyMult = getRequiredHackingLevel(ns, hostname) * getSecurityLevel(ns, hostname);
-  const player = getPlayer(ns);
+	const difficultyMult = getRequiredHackingLevel(ns, hostname) * getSecurityLevel(ns, hostname);
+	const player = getPlayer(ns);
 
-  const baseDiff = 500;
-  const baseSkill = 50;
-  const diffFactor = 2.5;
-  let skillFactor = diffFactor * difficultyMult + baseDiff;
-  skillFactor /= player.hacking + baseSkill;
+	const baseDiff = 500;
+	const baseSkill = 50;
+	const diffFactor = 2.5;
+	let skillFactor = diffFactor * difficultyMult + baseDiff;
+	skillFactor /= player.hacking + baseSkill;
 
-  const hackTimeMultiplier = 5;
-  const hackingTime =
-    (hackTimeMultiplier * skillFactor) /
-    (player.hacking_speed_mult * calculateIntelligenceBonus(player.intelligence, 1));
+	const hackTimeMultiplier = 5;
+	const hackingTime =
+		(hackTimeMultiplier * skillFactor) /
+		(player.hacking_speed_mult * calculateIntelligenceBonus(player.intelligence, 1));
 
-  return hackingTime * 1000;
+	return hackingTime * 1000;
 }
 function hackChance(ns, hostname) {
-  const player = getPlayer(ns);
+	const player = getPlayer(ns);
 
-  const hackFactor = 1.75;
-  const difficultyMult = (100 - getSecurityLevel(ns, hostname)) / 100;
-  const skillMult = hackFactor * player.hacking;
-  const skillChance = (skillMult - getRequiredHackingLevel(ns, hostname)) / skillMult;
-  const chance =
-    skillChance * difficultyMult * player.hacking_chance_mult * calculateIntelligenceBonus(player.intelligence, 1);
-  if (chance > 1) {
-    return 1;
-  }
-  if (chance < 0) {
-    return 0;
-  }
+	const hackFactor = 1.75;
+	const difficultyMult = (100 - getSecurityLevel(ns, hostname)) / 100;
+	const skillMult = hackFactor * player.hacking;
+	const skillChance = (skillMult - getRequiredHackingLevel(ns, hostname)) / skillMult;
+	const chance =
+		skillChance * difficultyMult * player.hacking_chance_mult * calculateIntelligenceBonus(player.intelligence, 1);
+	if (chance > 1) {
+		return 1;
+	}
+	if (chance < 0) {
+		return 0;
+	}
 
-  return chance;
+	return chance;
 }
 function hackPercent(ns, hostname) {
-  const player = getPlayer(ns);
+	const player = getPlayer(ns);
 
-  const balanceFactor = 240;
+	const balanceFactor = 240;
 
-  const difficultyMult = (100 - getSecurityLevel(ns, hostname)) / 100;
-  const skillMult = (player.hacking - (getRequiredHackingLevel(ns, hostname) - 1)) / player.hacking;
-  const percentMoneyHacked = (difficultyMult * skillMult * player.hacking_money_mult) / balanceFactor;
-  if (percentMoneyHacked < 0) {
-    return 0;
-  }
-  if (percentMoneyHacked > 1) {
-    return 1;
-  }
+	const difficultyMult = (100 - getSecurityLevel(ns, hostname)) / 100;
+	const skillMult = (player.hacking - (getRequiredHackingLevel(ns, hostname) - 1)) / player.hacking;
+	const percentMoneyHacked = (difficultyMult * skillMult * player.hacking_money_mult) / balanceFactor;
+	if (percentMoneyHacked < 0) {
+		return 0;
+	}
+	if (percentMoneyHacked > 1) {
+		return 1;
+	}
 
-  return percentMoneyHacked;
+	return percentMoneyHacked;
 }
+function growPercent(ns, hostname, threads, cores = 1) {
+	const p = getPlayer(ns);
+	const numServerGrowthCycles = Math.max(Math.floor(threads), 0);
+
+	//Get adjusted growth rate, which accounts for server security
+	const growthRate = /*CONSTANTS.ServerBaseGrowthRate*/ 1.03;
+	let adjGrowthRate = 1 + (growthRate - 1) / getSecurityLevel(ns, hostname);
+	if (adjGrowthRate > /*CONSTANTS.ServerMaxGrowthRate*/ 1.0035) {
+		adjGrowthRate = /*CONSTANTS.ServerMaxGrowthRate*/ 1.0035;
+	}
+
+	//Calculate adjusted server growth rate based on parameters
+	const serverGrowthPercentage = getGrowth(ns, hostname) / 100;
+	const numServerGrowthCyclesAdjusted =
+		numServerGrowthCycles * serverGrowthPercentage;
+
+	//Apply serverGrowth for the calculated number of growth cycles
+	const coreBonus = 1 + (cores - 1) / 16;
+	return Math.pow(adjGrowthRate, numServerGrowthCyclesAdjusted * p.hacking_grow_mult * coreBonus);
+}
+
+// stolen from the bitburner source code
+
+// so funny story right
+// we _could_ use cpu cores, and itd be more accurate
+// but getting the cpu cores on a server requires 2gb of ram
+// which simply is Way Too Fucking Much
+// so we dont
+// but we could
+// this game man
+function getWeakenAmount(threads, cpuCores = 1) {
+	return 0.05 * threads * cpuCores;
+}
+function numCycleForGrowth(ns, hostname, growth, cores = 1) {
+	const p = getPlayer(ns);
+
+	let ajdGrowthRate = 1 + (/*CONSTANTS.ServerBaseGrowthRate*/ 1.03 - 1) / getSecurityLevel(ns, hostname);
+	if (ajdGrowthRate > /*CONSTANTS.ServerMaxGrowthRate*/ 1.0035) {
+		ajdGrowthRate = /*CONSTANTS.ServerMaxGrowthRate*/ 1.0035;
+	}
+
+	const serverGrowthPercentage = getGrowth(ns, hostname) / 100;
+
+	const coreBonus = 1 + (cores - 1) / 16;
+	const cycles =
+		Math.log(growth) /
+		(Math.log(ajdGrowthRate) *
+			p.hacking_grow_mult *
+			serverGrowthPercentage *
+			coreBonus);
+
+	return cycles;
+}
+function getGrowFortifyAmount(ns, hostname, growth, threads, cores = 1) {
+	let usedCycles = numCycleForGrowth(ns, hostname, growth, cores);
+	usedCycles = Math.min(Math.max(0, Math.ceil(usedCycles)), threads);
+	return 2 * /*CONSTANTS.ServerFortifyAmount*/ 0.002 * usedCycles;
+}
+function getHackFortifyAmount(ns, hostname, hacked, threads) {
+	let maxThreadNeeded = Math.ceil((1 / hacked) * (getMoneyAvailable(ns, hostname) / getMaxMoneyAvailable(ns, hostname)));
+	if (isNaN(maxThreadNeeded)) {
+		// Server has a 'max money' of 0 (probably). We'll set this to an arbitrarily large value
+		maxThreadNeeded = 1e6;
+	}
+	return /*CONSTANTS.ServerFortifyAmount*/ 0.002 * Math.min(threads, maxThreadNeeded);
+}
+
 
 // cached function variants to avoid || () = type nonsense
 function getPlayer(ns) {
 	return playerCache || (playerCache = ns.getPlayer());
 }
+function getGrowth(ns, hostname) {
+	return growthCache[hostname] || (growthCache[hostname] = ns.getServerGrowth(hostname));
+}
+function getMinSecurityLevel(ns, hostname) {
+	return securityMinLevelCache[hostname] || (securityMinLevelCache[hostname] = ns.getServerMinSecurityLevel(hostname));
+}
+let oldLevel = {};
 function getSecurityLevel(ns, hostname) {
-	return securityLevelCache[hostname] || (securityLevelCache[hostname] = ns.getServerSecurityLevel(hostname));
+	if (securityLevelCache[hostname]) return securityLevelCache[hostname];
+	let level = (securityLevelCache[hostname] = ns.getServerSecurityLevel(hostname));
+	if (oldLevel[hostname] !== level) {
+		oldLevel[hostname] = level;
+		securityLevelScheduled = securityLevelScheduled.filter(l => l[0] !== hostname);
+	}
+	return level + (securityLevelScheduled.filter(l => t >= l[2] && hostname === l[0]).reduce((p, n) => p + n[1], 0) || 0);
 }
 function getRequiredHackingLevel(ns, hostname) {
 	return requiredHackingLevel[hostname] || (requiredHackingLevel[hostname] = ns.getServerRequiredHackingLevel(hostname));
 }
+let oldMoney = {};
 function getMoneyAvailable(ns, hostname) {
-	return moneyAvailableCache[hostname] || (moneyAvailableCache[hostname] = ns.getServerMoneyAvailable(hostname));
+	if (moneyAvailableCache[hostname]) return moneyAvailableCache[hostname];
+	let money = (moneyAvailableCache[hostname] = ns.getServerMoneyAvailable(hostname));
+	if (oldMoney[hostname] !== money) {
+		oldMoney[hostname] = money;
+		moneyAvailableScheduled = moneyAvailableScheduled.filter(l => l[0] !== hostname);
+	}
+	return money + (moneyAvailableScheduled.filter(l => t >= l[2] && hostname === l[0]).reduce((p, n) => p + n[1], 0) || 0);
 }
 function getMaxMoneyAvailable(ns, hostname) {
 	return moneyMaxCache[hostname] || (moneyMaxCache[hostname] = ns.getServerMaxMoney(hostname));
@@ -231,7 +327,12 @@ function mix(a, b, x) {
 	return a * (1 - x) + b * x;
 }
 
+let avgThreadAmount = 1;
+/** @param {NS} ns **/
 export async function loop(ns) {
+	t += Date.now() - lastt;
+	lastt = Date.now();
+
 	let log = [];
 	let stats = [];
 
@@ -240,6 +341,7 @@ export async function loop(ns) {
 	hackChanceCache = {};
 	hackTimeCache = {};
 	securityLevelCache = {};
+	growthCache = {};
 	playerCache = undefined;
 
 	let smallestTimeToWait = updateInterval;
@@ -275,9 +377,9 @@ export async function loop(ns) {
 	let ranHack = 0;
 	let threadCounts = [];
 
-	let rootedPadAmt = rooted.reduce((s1, s2) => {return Math.max(s1 || 0, s2.length || 0)});
+	let rootedPadAmt = rooted.reduce((s1, s2) => { return Math.max(s1 || 0, s2.length || 0) });
 	let commandPadAmt = 6; // weaken is 6 chars long
-	let sortedPadAmt = sorted.reduce((s1, s2) => {return Math.max(s1 || 0, s2.length || 0)});
+	let sortedPadAmt = sorted.reduce((s1, s2) => { return Math.max(s1 || 0, s2.length || 0) });
 
 	rootedPadAmt = Math.min(rootedPadAmt, maxServerChars);
 	sortedPadAmt = Math.min(sortedPadAmt, maxServerChars);
@@ -298,14 +400,14 @@ export async function loop(ns) {
 			if (!targetServer) break;
 
 			const hackChance = getHackChance(ns, targetServer);
-			const minMoneyAmt = hackPercent(ns, targetServer) * getMaxMoneyAvailable(ns, targetServer);
+			const minMoneyAmt = hackPercent(ns, targetServer) * avgThreadAmount * getMaxMoneyAvailable(ns, targetServer);
 
 			let command;
 			let time;
-			if (hackChance < minChance) {
+			if (hackChance < minChance && getMinSecurityLevel(ns, targetServer) !== getSecurityLevel(ns, targetServer)) {
 				command = 'weaken.script';
 				time = getWeakenTime(ns, targetServer);
-			} else if (getMoneyAvailable(ns, targetServer) < minMoneyAmt) { // penis
+			} else if (getMoneyAvailable(ns, targetServer) < minMoneyAmt) {
 				command = 'grow.script';
 				time = getGrowTime(ns, targetServer);
 			} else {
@@ -320,10 +422,58 @@ export async function loop(ns) {
 			let threads = Math.min(Math.floor(availram / scriptram), maxThreads);
 			if (scriptram > availram) break;
 
+			if (command === 'weaken.script') {
+				// optimize the amount of threads when weakening
+				// to avoid redundant weakens
+				while (getMinSecurityLevel(ns, targetServer) > getSecurityLevel(ns, targetServer) - getWeakenAmount(threads)) {
+					threads--;
+				}
+				if (threads === 0) threads = 1; // this would create a redundant weaken, but theres not much better handling for this possible in this codebase
+			}
+
 			// time /= threads; // that's not even true
 
 			const pid = ns.exec(command, fromServer, threads, targetServer);
 			if (pid === 0) break;
+
+			// schedule stuff! :)
+			switch (command) {
+				case 'weaken.script':
+					securityLevelScheduled.push([
+						targetServer,
+						-getWeakenAmount(threads),
+						// we schedule this earlier by the hack time since the weaken will apply once the hack is complete
+						// Hash Tag Max Efficiency
+						// https://media.discordapp.net/attachments/415207508303544323/924114194817765466/Concept_of_Batches.png
+						(getWeakenTime(ns, targetServer) - getHackTime(ns, targetServer) + 100) / 1000
+					]);
+					break;
+				case 'grow.script':
+					moneyAvailableScheduled.push([
+						targetServer,
+						(getMoneyAvailable(ns, targetServer) * growPercent(ns, targetServer, threads)) - getMoneyAvailable(ns, targetServer),
+						(getGrowTime(ns, targetServer) - getHackTime(ns, targetServer) + 100) / 1000
+					]);
+					securityLevelScheduled.push([
+						targetServer,
+						getGrowFortifyAmount(ns, targetServer, growPercent(ns, targetServer, threads), threads),
+						(getGrowTime(ns, targetServer) - getWeakenTime(ns, targetServer) + 100) / 1000
+					]);
+					break;
+				case 'hack.script':
+					securityLevelScheduled.push([
+						targetServer,
+						getHackFortifyAmount(ns, targetServer, hackPercent(ns, targetServer) * threads, threads),
+						(getHackTime(ns, targetServer) - getWeakenTime(ns, targetServer) + 100) / 1000
+					]);
+					moneyAvailableScheduled.push([
+						targetServer,
+						(getMoneyAvailable(ns, targetServer) * hackPercent(ns, targetServer) * threads) - getMoneyAvailable(ns, targetServer),
+						(getGrowTime(ns, targetServer) - getHackTime(ns, targetServer) + 100) / 1000
+					]);
+					break;
+			}
+
 			smallestTimeToWait = Math.min(smallestTimeToWait, time);
 
 			command = command.split('.')[0];
@@ -355,11 +505,12 @@ export async function loop(ns) {
 	}
 
 	let avgThreadCount = threadCounts.reduce((p, c) => p + c) / threadCounts.length;
+	avgThreadAmount = avgThreadCount; // for use in next tick
 	processesPerTick = Math.max(mix(processesPerTick, 0, smallestTimeToWait / 1000 * 0.25), totalStartedScripts);
 
 	stats.push(`${totalRunningScripts} processes running on ${rooted.length} servers hacking ${sorted.length} servers out of ${res[0].length + res[1].length} known`);
 	stats.push(`${totalStartedScripts} processes (${Math.floor(totalStartedScripts / totalRunningScripts * 1000) / 10}%) started this tick`);
-	stats.push(`${ranHack} hacking, ${ranWeaken} weakening, ${ranGrow} growing`);
+	stats.push(`${ranHack} hacking, ${ranWeaken} weakening, ${ranGrow} growing; ${securityLevelScheduled.length} + ${moneyAvailableScheduled.length} changes scheduled`);
 	stats.push(`${Math.floor(avgThreadCount * 10) / 10} threads avg, ${Math.floor(totalRunningScripts / sorted.length * 10) / 10} processes/server, ${Math.floor(processesPerTick / (smallestTimeToWait / 1000) * 100) / 100} processes/s`);
 
 	return [log, stats, smallestTimeToWait];
@@ -378,7 +529,7 @@ export async function main(ns) {
 
 	while (true) {
 		ns.clearLog();
-		
+
 		let [log, stats, time] = await loop(ns);
 		logs.push(...log);
 		if (logs.length > maxLogLength) logs = logs.slice(-maxLogLength);
